6797b7c3865e90c72262b66afab85c1f8eb31217
bbangwon <bbangwon.chung@gmail.com>
2023-03-06 00:56:20 +0900

    현재까지 작업 커밋

---
+++ MauiAppTest/XamlCode/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics;
+
+namespace MauiAppTest.XamlCode;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        Debug.WriteLine("Clicked !");
+    }
+}
