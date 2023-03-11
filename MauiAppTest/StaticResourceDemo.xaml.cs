28d32b55722e2204cc026db6588cb68c2d6c1500
youngil <youngil@placeb.com>
2023-03-11 19:38:35 +0900

    DynamicResource

--- MauiAppTest/StaticResourceDemo.xaml.cs
+++ MauiAppTest/StaticResourceDemo.xaml.cs
@@ -7,18 +7,41 @@ public partial class StaticResourceDemo : ContentPage
 		InitializeComponent();
 	}
 
-    private void GotoCustom(object sender, EventArgs e)
+    private async void GotoCustom(object sender, EventArgs e)
     {
-
+        await Shell.Current.GoToAsync(nameof(SharedResourceDemo));
     }
 
     private void OnLight(object sender, EventArgs e)
     {
+        /*
+        LayoutRoot.Background = Colors.Silver;
 
+        tipLabel.TextColor = Colors.Navy;
+        billLabel.TextColor = Colors.Navy;
+        totalLabel.TextColor = Colors.Navy;
+        tipOutput.TextColor = Colors.Navy;
+        totalOutput.TextColor = Colors.Navy;
+        */
+
+        //Dynamic Resources
+        Resources["fgColor"] = Colors.Navy;
+        Resources["bgColor"] = Colors.Silver;
     }
     private void OnDark(object sender, EventArgs e)
     {
+        /*
+        LayoutRoot.Background = Colors.Navy;
 
+        tipLabel.TextColor = Colors.Silver;
+        billLabel.TextColor = Colors.Silver;
+        totalLabel.TextColor = Colors.Silver;
+        tipOutput.TextColor = Colors.Silver;
+        totalOutput.TextColor = Colors.Silver;
+        */
+
+        Resources["fgColor"] = Colors.Silver;
+        Resources["bgColor"] = Colors.Navy;
     }
 
 }
