5bcf34e4e5c50e123c28a1d8c2f36acd72151c68
youngil <youngil@placeb.com>
2023-03-11 13:25:36 +0900

    StaticResourceDemo

---
+++ MauiAppTest/StaticResourceDemo.xaml.cs
@@ -0,0 +1,24 @@
+namespace MauiAppTest;
+
+public partial class StaticResourceDemo : ContentPage
+{
+	public StaticResourceDemo()
+	{
+		InitializeComponent();
+	}
+
+    private void GotoCustom(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OnLight(object sender, EventArgs e)
+    {
+
+    }
+    private void OnDark(object sender, EventArgs e)
+    {
+
+    }
+
+}
